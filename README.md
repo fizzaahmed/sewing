b5bb45eee1eb57e6b5922a0ca0c094de4ce1ac23
fizzaahmed <fiahmed@umich.edu>
2021-10-04 16:20:19 -0400

    Creating main class

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sewing
+An easy to use calculator to determine yardage for common sewing projects. 
+Items include:
+-Skater skirt
+-Shorts
+-Small fitted top
+-Looser top
+
---
+++ src/main.py
@@ -0,0 +1,13 @@
+'''
+Calcuates yards needed for my projects. 
+Will take into consideration the way that pieces need to be cut out.
+'''
+
+class PatternCalculator:
+    def init(self):
+        # measurements
+        self.waist = 0
+        self.hip = 0
+        self.leg_length = 0
+        self.bust = 0
+        self.lower_bust = 0
